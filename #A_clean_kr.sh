0d5c7203ffc94c9c07ebfcc34252fc0b7ff5fcd7
gustye <53995658+gustye@users.noreply.github.com>
2021-02-17 04:43:53 +0800

    Add files via upload

---
+++ #A_clean_kr.sh
@@ -0,0 +1,153 @@
+#!/system/bin/sh
+echo "==================================="
+date
+echo "==================================="
+echo " "
+echo " ╔═════．．．．．．．．．．．．．．．．．．．． ．． ═════╗"
+echo "  ．．．．🇵 🇺 🇧 🇬  🇲 🇴 🇧 🇮 🇱 🇪  🇭 🇦 🇨 🇰．．"
+echo " ╚═════．．．．．．．．．．．．．．．．．．．．．．．═════╝"
+echo " "
+echo " ╔═════．．．．．．．．．．．．．．．．．．．．．．．═════╗"
+echo " ．．ＡＥＸ ＰＲＯＪＥＣＴ ＰＵＢＧ   ０．１８．０．．"
+echo " ╚═════．．．．．．．．．．．．．．．．．．．．．．．═════╝"
+echo " "
+echo " ╔═════．．．．．．．．．．．．．．．．．．．．．．．═════╗"
+echo " ．．．．ＭＥＤＩＵＭ ＣＬＥＡＮ ＰＵＢＧ ＫＯＲＥＡ．．"
+echo " ╚═════．．．．．．．．．．．．．．．．．．．．．．．═════╝"
+echo " "
+echo " "
+echo " ╔═════．．．．．．．．．．．．．．．．．．．．．．．═════╗"
+echo " Ｐｒｏｓｓｅｓｉｎｇ．．"
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/DCIM/.thumbnails
+rm -rf /storage/emulated/0/DCIM/.yyy
+rm -rf /storage/emulated/0/.ColombiaMedia
+rm -rf /storage/emulated/0/.config
+rm -rf /storage/emulated/0/.profig.os
+rm -rf /storage/emulated/0/.com.taobao.dp
+rm -rf /storage/emulated/0/.DataStorage
+rm -rf /storage/emulated/0/.Uc2DataStorage
+rm -rf /storage/emulated/0/.Uc2UTSystemConfig
+rm -rf /storage/emulated/0/.UTSystemConfig
+rm -rf /storage/emulated/0/.zjnb
+rm -rf /storage/emulated/0/.cc
+rm -rf /storage/emulated/0/.um
+rm -rf /storage/emulated/0/.uxx
+rm -rf /storage/emulated/0/.a.dat
+rm -rf /storage/emulated/0/amap
+rm -rf /storage/emulated/0/backups/.SystemConfig
+rm -rf /storage/emulated/0/backups/system
+rm -rf /storage/emulated/0/UCTurbo/.apolloCache
+rm -rf /storage/emulated/0/UCTurbo/log
+rm -rf /storage/emulated/0/UCTurbo/tmp
+rm -rf /storage/emulated/0/UCTurbo/us
+rm -rf /storage/emulated/0/Android/data/coordinate
+rm -rf /storage/emulated/0/Android/data/com.android.gallery3d/cache
+rm -rf /storage/emulated/0/Android/data/com.camerasideas.instashot/cache
+rm -rf /storage/emulated/0/Android/data/com.android.providers.media/albumthumbs
+rm -rf /storage/emulated/0/Android/data/com.instagram.android/cache
+rm -rf /storage/emulated/0/Android/data/com.instagram.android/files
+rm -rf /storage/emulated/0/Android/data/com.mxtech.videoplayer.ad/cache
+rm -rf /storage/emulated/0/Android/data/com.ucturbo/cache
+rm -rf /storage/emulated/0/Android/data/com.sonyericsson.music/files/cache
+rm -rf /storage/emulated/0/Android/data/com.sonyericsson.music/files/album_images
+rm -rf /storage/emulated/0/Android/data/com.snaptube.premium/files
+rm -rf /storage/emulated/0/Android/data/org.telegram.messenger/cache
+rm -rf /storage/emulated/0/Android/data/org.telegram.plus/cache
+rm -rf /storage/emulated/0/Android/data/com.mi.android.globalminusscreen/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.global.packageinstaller/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.videoplayer/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.securitycenter/cache
+rm -rf /storage/emulated/0/Android/data/com.android.thememanager/cache
+rm -rf /storage/emulated/0/Android/data/com.google.android.apps.maps/cache
+rm -rf /storage/emulated/0/Android/data/com.google.android.youtube/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.cleanmaster/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.gallery/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.player/cache
+rm -rf /storage/emulated/0/Android/data/com.vmos.gbi/cache
+rm -rf /storage/emulated/0/Android/data/com.xiaomi.mipicks/cache
+rm -rf /storage/emulated/0/Android/data/org.joa.zipperplus/cache
+rm -rf /storage/emulated/0/Android/data/com.miui.screenrecorder/cache
+rm -rf /storage/emulated/0/MidasOversea
+rm -rf /storage/emulated/0/Music/.qqq
+rm -rf /storage/emulated/0/MIUI/.cache
+rm -rf /storage/emulated/0/MIUI/debug_log
+rm -rf /storage/emulated/0/MIUI/sound_recorder
+rm -rf /storage/emulated/0/MIUI/Video/thumb
+rm -rf /storage/emulated/0/MIUI/Gallery/cloud
+rm -rf /storage/emulated/0/tencent
+rm -rf /storage/emulated/0/InShot/.cache
+rm -rf /storage/emulated/0/images2
+rm -rf /storage/emulated/0/InShot/.disk_cache
+rm -rf /storage/emulated/0/InShot/.crash
+rm -rf /storage/emulated/0/InShot/.log
+rm -rf /storage/emulated/0/Pictures/.thumbnails
+rm -rf /storage/emulated/0/Movies/.thumbnails
+rm -rf /storage/emulated/0/Music/.thumbnails
+sleep 1
+echo " ｃｏｍ．ｐｕｂｇ．ｋｒｍｏｂｉｌｅ "
+rm -rf /storage/emulated/0/Android/.system_android_l2
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache_
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/.fff
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/.system_android_l2
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ca-bundle.pem
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/cacheFile.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/vmpcloudconfig.json*
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Intermediate
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs0/puffer_res.eifs
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs1/puffer_res.eifs
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/coverversion.ini
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Activity
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Arena
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Character
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Crate
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Download
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Friend
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Pet
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/PersonSpace
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/PufferDownload
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/RP
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/UnknowPass
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Chat
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/GEM
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Notice
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Season
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Store
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Task
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/pandora
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/XMission
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/DIY
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/SocialIsland
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/loginInfoFile.json
+sleep 1
+rm -rf /data/user/0/com.pubg.krmobile/cache_
+rm -rf /data/user/0/com.pubg.krmobile/app_appcache
+rm -rf /data/user/0/com.pubg.krmobile/app_databases
+rm -rf /data/user/0/com.pubg.krmobile/app_geolocation
+rm -rf /data/user/0/com.pubg.krmobile/app_bugly_
+rm -rf /data/user/0/com.pubg.krmobile/app_crashrecord_
+rm -rf /data/user/0/com.pubg.krmobile/databases
+rm -rf /data/user/0/com.pubg.krmobile/code_cache
+rm -rf /data/user/0/com.pubg.krmobile/no_backup
+rm -rf /data/user/0/com.pubg.krmobile/app_tbs
+rm -rf /data/user/0/com.pubg.krmobile/app_textures
+rm -rf /data/user/0/com.pubg.krmobile/app_webview
+rm -rf /data/user/0/com.pubg.krmobile/app_webview_imsdk_inner_webview
+rm -rf /data/user/0/com.pubg.krmobile/files/.iii
+rm -rf /data/user/0/com.pubg.krmobile/files/.system_android_l2
+rm -rf /data/user/0/com.pubg.krmobile/files/.TwitterKit
+rm -rf /data/user/0/com.pubg.krmobile/files/iMSDK
+rm -rf /data/user/0/com.pubg.krmobile/files/com.tencent.gcloud.gvoice
+sleep 1
+echo " Ｄｅｌｅｔｅ ｆｉｌｅ.ｊｓｏｎ ａｎｄ ｆｉｌｅ.ｌｏｇ．．．"
+find /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/ . -name '*.json' -type f -delete
+find /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/ . -name '*.log' -type f -delete
+sleep 2
+echo " ╚═════．．．．．．．．．．．．．．．．．．．．．．．═════╝"
+echo "================================================================================="
+echo ".               Ａｌｌ Ｐｒｏｓｓｅｓ Ｓｕｃｃｅｓ "
+echo "================================================================================="
+sleep 1
